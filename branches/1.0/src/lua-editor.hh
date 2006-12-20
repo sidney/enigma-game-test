428b0996457db812abfac3c3fbb94b1fd1ca11e4
ral <ral@1efed42d-d900-4f75-af85-68d09028c2bd>
2006-12-20 23:30:31 +0000

    - create branch 1.0 for 1.00 compatibility development - trunk progresses to 1.10 compatibility development

---
+++ branches/1.0/src/lua-editor.hh
@@ -0,0 +1,8 @@
+/*
+** Lua binding: editor
+** Generated automatically by tolua++-1.0.92 on Wed Sep 27 01:52:17 2006.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_editor_open (lua_State* tolua_S);
+
