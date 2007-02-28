cf3f2218e6d7d512d76d2cdba564426f40cd1a0a
ral <ral@1efed42d-d900-4f75-af85-68d09028c2bd>
2007-02-28 22:47:04 +0000

    Homepage: - some mods and adds to English FAQ - removed 1.00 beta announcement from main page

--- homepage/input/output-files.lua
+++ homepage/input/output-files.lua
@@ -7,7 +7,7 @@ newsdir = "news/"
 suffix = ".html"
 
 language_list = {"", "_de"} -- "_fr"
-newsfield = {15, 16, 17, 18}
+newsfield = {16, 17, 18}
 
 general = {
     infile = directory.."schema"..suffix,
