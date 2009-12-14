50cf2e7ed330575d6d46a45133d5d325319dc779
ral <ral@1efed42d-d900-4f75-af85-68d09028c2bd>
2009-12-14 19:00:47 +0000

    Homepage: - announcement of Enigma 1.01 Windows 7

--- homepage/input/output-files.lua
+++ homepage/input/output-files.lua
@@ -10,7 +10,7 @@ language_list = {"", "_de", "_ru"} -- "_es", "_fr"
 
 -- The newsfield declares the news (by number) to be shown on the main page.
 
-newsfield = {21, 22, 23, 24, 26}
+newsfield = {21, 22, 23, 24, 26, 27}
 
 -- General $$mystring$$-macros. Strings are taken as-is, tables are
 -- constellations of html input files, functions are executed, with the
