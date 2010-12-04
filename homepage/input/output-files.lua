f369d4bb8c1ca7ec18cb66ce1f43429b92a7e0a4
andreasl <andreasl@1efed42d-d900-4f75-af85-68d09028c2bd>
2010-12-04 23:17:56 +0000

    Advent 2010: Day 5. Also highlight how to update the advent pack.
    Note:
     - The paragraph in advent_2010 which describes the update-process
       has been slightly reworked, and essential parts are now in bold
       font, to emphasize them - though only in English and German yet.

--- homepage/input/output-files.lua
+++ homepage/input/output-files.lua
@@ -525,14 +525,14 @@ html.advent_2010_day4 = {
     body = {"advent_2010/advent_day4", "longline"}
 }
 
--- html.advent_2010_day5 = {
---     outfile = "advent_day5.html",
---     title = "Advent 2010",
---     title_de = "Advent 2010",
---     title_ru = "Рождественский календарь 2010",
---     title_es = "",
---     body = {"advent_2010/advent_day5", "longline"}
--- }
+html.advent_2010_day5 = {
+    outfile = "advent_day5.html",
+    title = "Advent 2010",
+    title_de = "Advent 2010",
+    title_ru = "Рождественский календарь 2010",
+    title_es = "",
+    body = {"advent_2010/advent_day5", "longline"}
+}
 
 -- html.advent_2010_day6 = {
 --     outfile = "advent_day6.html",
